c42bf155754d3ff2c10b22d6e0a73a3fbe770523
Imane Kadi <imanemnds@gmail.com>
2021-10-25 15:23:48 +0200

    Interface Somme

---
+++ app/src/main/java/TD2_PF/Somme.java
@@ -0,0 +1,16 @@
+package TD2_PF;
+
+@FunctionalInterface
+
+public interface Somme<T> {
+
+    T somme(T var1, T var2);
+
+    Somme<Integer> sommeInt = new Somme<Integer>() {
+        @Override
+        public Integer somme(Integer var1, Integer var2) {
+            return null;
+        }
+    };
+
+}
