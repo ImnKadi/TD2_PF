43564924493555118028ff31634d130cfbdbdb1d
Imane Kadi <imanemnds@gmail.com>
2021-10-25 17:12:52 +0200

    Début Qst 2 Exo 2

--- app/src/main/java/TD2_PF/App.java
+++ app/src/main/java/TD2_PF/App.java
@@ -9,34 +9,34 @@ import java.util.function.Predicate;
 
 public class App {
 
-    public static void QST1(){
+    public static void QST1() {
 
         System.out.print("Somme et Sommable");
         // Somme des 2 entiers
-        Somme<Integer> integerSomme=(var1, var2)-> var1 + var2;
-        System.out.println(integerSomme.somme(20,20));
+        Somme<Integer> integerSomme = (var1, var2) -> var1 + var2;
+        System.out.println(integerSomme.somme(20, 20));
 
         // Somme des 2 doubles
-        Somme<Double> doubleSomme=((var1, var2) -> var1+var2);
-        System.out.println(doubleSomme.somme(20.0,20.0));
+        Somme<Double> doubleSomme = ((var1, var2) -> var1 + var2);
+        System.out.println(doubleSomme.somme(20.0, 20.0));
         // Somme des 2 longs
 
-        Somme<Long> longSomme=(var1,var2) -> (var1+var2);
-        System.out.println(longSomme.somme(1L,1L));
+        Somme<Long> longSomme = (var1, var2) -> (var1 + var2);
+        System.out.println(longSomme.somme(1L, 1L));
 
         // Somme des 2 Strings
 
-        Somme<String> stringSomme=(var1, var2) -> (var1+var2);
-        System.out.println(stringSomme.somme("un","deux"));
+        Somme<String> stringSomme = (var1, var2) -> (var1 + var2);
+        System.out.println(stringSomme.somme("un", "deux"));
     }
 
-    public static void QST2(){
+    public static void QST2() {
 
         final List<String> l2s = List.of("Un", "deux");
-        final Map<String, Integer> m2s =  Map.of("e1", 1, "e2", 2);
+        final Map<String, Integer> m2s = Map.of("e1", 1, "e2", 2);
     }
 
-    public static void QST3(){
+    public static void QST3() {
 
         System.out.println("Function convertit un type en un autre type et prend en argument type T");
         System.out.println("Predicate est de type booléen teste un argument et renvoie vrai ou faux. Elle est du type T");
@@ -45,21 +45,35 @@ public class App {
     }
 
 
-    public static void EXO2QST1(){
+    public static void EXO2QST1() {
 
-        Predicate<Integer> tropPetit = taille -> taille <100;
-        Predicate<Integer> tropGrand = taille -> taille >200;
+        Predicate<Integer> tropPetit = taille -> taille < 100;
+        Predicate<Integer> tropGrand = taille -> taille > 200;
         Predicate<Integer> TailleIncorrecte = tropPetit.or(tropGrand);
         Predicate<Integer> TailleCorrecte = TailleIncorrecte.negate();
         Predicate<Double> tropLourd = poids -> poids > 150.0;
         Predicate<Double> PoidsIncorrecte = tropLourd;
         Predicate<Double> PoidsCorrecte = tropLourd.negate();
-
-
-
-
-
     }
+
+
+    // Exo2QST2
+    public static class Paire<T, U> {
+        public T fst;
+        public U snd;
+
+        public Paire(T fst, U snd) {
+            this.fst = fst;
+            this.snd = snd;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s,%s)", fst.toString(), snd.toString());
+        }
+    }
+
+
 
 
 }
